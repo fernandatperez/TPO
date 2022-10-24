97417173ca78a1c44ed6b9616280132f7c6674d4
Fernanda <fernanda.tperez@outlook.com>
2022-10-23 21:07:04 -0300

    modifico los links a los html y consumo api

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 'use strict'
-
+//* alerta para cuando se agrega un producto al carrito de compras *//
 function alertaCompra() {
     alert("Producto agregado al carrito");
 }
 
+//* funcion que realiza la validacion de los campos del formulario en solicitarPresupuesto.html *//
 function validar(){
 
-var todo_correcto = true;
+//* seteo todas las variables booleanas en true para luego setearlas en false si no pasan la validacion *//
 var validoNombre = true;
 var validoNumero = true;
 var validoConsulta = true;
 
-
+//*valido que el campo nombre tenga contenido *//
 if(document.getElementById('nombre').value.length < 1 ){
     validoNombre = false;
 }
+//* valido que el contenido del campo de numero de telefono sea not a number *//
 if(isNaN(document.getElementById('numero').value)){
     validoNumero = false;
 }
+//* valido que el campo consulta tenga contenido *//
 if(document.getElementById('consulta').value.length < 1 ){
     validoConsulta = false;
 }
 
-
+//* acá se realizan los alert correspondientes a los campos que no pasen la validación *//
 if(!validoNumero){
     alert('Por favor ingrese un numero de telefono válido');
 } else {
@@ -33,6 +36,7 @@ if(!validoNumero){
     } 
 } 
 
+//* aca se realiza el alert en caso que se hayan superado todas las validaciones *//
 if(validoConsulta && validoNombre && validoNumero){
     alert('formulario enviado exitosamente');
 }
@@ -41,32 +45,34 @@ return true;
 }
 
 
+//* en este script se resuelve el carrousel que a demás de moverse, tiene habilitados botones *//
+//* para que el usuario pueda navegar por ellos *//
 const grande    = document.querySelector('.grande')
 const punto     = document.querySelectorAll('.punto')
 
 
-// Cuando CLICK en punto
-    // Saber la posición de ese punto
-    // Aplicar un transform translateX al grande
-    // QUITAR la clase activo de TODOS puntos
-    // AÑADIR la clase activo al punto que hemos hecho CLICK
+// Cuando se hace CLICK en punto
+    // se extrae la posición de ese punto
+    // se aplica un transform translateX al objeto .grande
+    // se quita la clase activo de TODOS puntos
+    // se añade la clase activo al punto que hemos hecho CLICK
 
-// Recorrer TODOS los punto
+// acá recorremos TODOS los puntos
 punto.forEach( ( cadaPunto , i )=> {
     // Asignamos un CLICK a cadaPunto
     punto[i].addEventListener('click',()=>{
 
-        // Guardar la posición de ese PUNTO
+        // Guardamos la posición de ese PUNTO
         let posicion  = i
-        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        // Calculamos el espacio que debe DESPLAZARSE el GRANDE
         let operacion = posicion * -(100/3)
 
-        // MOVEMOS el grand
+        // MOVEMOS el grande
         grande.style.transform = `translateX(${ operacion }%)`
 
-        // Recorremos TODOS los punto
+        // Recorremos TODOS los puntos
         punto.forEach( ( cadaPunto , i )=>{
-            // Quitamos la clase ACTIVO a TODOS los punto
+            // Quitamos la clase ACTIVO a TODOS los puntos
             punto[i].classList.remove('activo')
         })
         // Añadir la clase activo en el punto que hemos hecho CLICK
@@ -74,3 +80,24 @@ punto.forEach( ( cadaPunto , i )=> {
 
     })
 })
+
+// detecto el momento en el que se realiza scroll y a través del ID ip llamo a la funcion que 
+//hace uso de la api para obtener el pais del IP en cuestion que entro a visitar la pagina 
+
+document.getElementById("ip").addEventListener("scroll", obtenerIP);
+
+//funcion que hace uso de la API https://ipwhois.io/
+function obtenerIP(){
+var ip  // declaro laq variable que va a contener la IP actual
+var XMLHttp = new XMLHttpRequest();
+XMLHttp.onreadystatechange = function() {
+	if(this.readyState == 4 && this.status == 200) {
+		var ipwhois = JSON.parse(this.responseText);
+		console.log(ipwhois.country + ' ' + ipwhois.flag.emoji); // Output: United States 🇺🇸
+        alert("pais de donde se visita la pagina"+ipwhois.country)
+    }
+};
+XMLHttp.open('GET', 'https://ipwho.is/' + ip, true);
+XMLHttp.send();
+return true;
+}
